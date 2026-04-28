5f379b770de041c88e05346edfc41f407c666a52
YAZAN US <yazanus94@gmail.com>
2026-04-28 15:52:12 +0300

    Added Cypress Task 2 tests

--- cypress.config.js
+++ cypress.config.js
@@ -2,11 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   projectId: '5v5h42',
-  allowCypressEnv: false,
-
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl: "https://automationexercise.com",
   },
 });
